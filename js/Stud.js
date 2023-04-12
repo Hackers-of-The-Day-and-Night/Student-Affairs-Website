4d3113a32ef92992b7effebfe2157fb9f18280d5
Ahmed Shaaban <ahmedlearningscience@gmail.com>
2023-04-12 06:44:34 +0200

    Add Stud class

---
+++ js/Stud.js
@@ -0,0 +1,24 @@
+class Stud {
+  id;
+  name;
+  email;
+  phone;
+  dob;
+  gender;
+  gpa;
+  level;
+  department;
+  status;
+  constructor(id, name, email, phone, dob, gender, gpa, level, department, status) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.dob = dob;
+    this.gender = gender;
+    this.gpa = gpa;
+    this.level = level;
+    this.department = department;
+    this.status = status;
+  }
+}
