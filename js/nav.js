36c6c2fc3f00a2aecc2c8eceab0379df7709c10e
Ahmed Shaaban <ahmedlearningscience@gmail.com>
2023-04-12 09:13:20 +0200

    Enhance nav design with - active class will be set for the current page

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,16 @@
 let nav = document.querySelector('nav');
 nav.innerHTML = "\
-    <ul>\
-        <li><a href='index.html'>Home</a></li>\
-        <li><a href='add.html'>Add New</a></li>\
-        <li><a href='view.html'>View/Edit</a></li>\
-        <li><a href='search_active.html'>Assign Department</a></li>\
-    </ul>\
+  <ul>\
+    <li><a href='index.html?pid=0'>Home</a></li>\
+    <li><a href='add.html?pid=1'>Add New</a></li>\
+    <li><a href='view.html?pid=2'>View/Edit</a></li>\
+    <li><a href='search_active.html?pid=3'>Assign Department</a></li>\
+  </ul>\
 ";
+
+let url = new URLSearchParams(location.search);
+let pid = url.get('pid');
+if (pid) {
+  let links = document.querySelectorAll("nav ul li");
+  links[pid].classList.add('active');
+}
